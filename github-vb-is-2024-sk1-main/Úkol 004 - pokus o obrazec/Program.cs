da3b8dec6fce41f5d362c528ce78acd4afeffd48
Vladan Brandýský <vladan.brandysky@tul.cz>
2024-10-29 22:45:22 +0100

    Add files via upload

---
+++ github-vb-is-2024-sk1-main/Úkol 004 - pokus o obrazec/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+class Obrazce
+{
+    static void Main()
+    {
+        string again = "a";
+        while (again == "a")
+        {
+            Console.Clear();
+            Console.WriteLine("Zadejte číslo, nebo něco, to je jedno.");
+            
+            int velikost;
+            while (!int.TryParse(Console.ReadLine(), out velikost) || velikost < 0)
+            {
+                Console.WriteLine("Nezadal jste číslo, zadejte prosím celé číslo, nebo něco, ale spíš to číslo.");
+            }
+
+            Console.WriteLine("\nŠachovnice");
+            for (int i = 0; i < velikost; i++)
+            {
+                for (int j = 0; j < velikost; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                    Thread.Sleep(100);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Pro opakování programu stiskněte klávesu 'a', jinak libovolnou jinou klávesu pro ukončení.");
+            again = Console.ReadLine().ToLower();
+        }
+    }
+}
