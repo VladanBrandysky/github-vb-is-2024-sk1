376c4e07cf11ba3bc911f4f6b58b04f1e4230fc5
VladanBrandysky <vladan.brandysky@tul.cz>
2024-11-13 09:46:48 +0100

    notak

--- obj/Debug/net8.0/github-vb-is-2024-sk1.AssemblyInfo.cs
+++ obj/Debug/net8.0/github-vb-is-2024-sk1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("github-vb-is-2024-sk1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aa1706b183aa03f11be97d86580977efdebf33a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c9344327627d66e3678baf4585b96b24da092dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("github-vb-is-2024-sk1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("github-vb-is-2024-sk1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
